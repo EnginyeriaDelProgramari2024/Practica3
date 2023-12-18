c8d4a845a80bfa24de70349058c8a9c2731ba2b0
bk203esd <bk203esd@gmail.com>
2023-12-18 11:44:06 +0100

    implemented BiometricData.java

--- src/data/BiometricData.java
+++ src/data/BiometricData.java
@@ -1,4 +1,19 @@
 package data;
 
 public class BiometricData {
+    private SingleBiometricData facialBiometricData;
+    private SingleBiometricData fingerprintBiometricData;
+
+    public BiometricData(SingleBiometricData facialBiometricData, SingleBiometricData fingerprintBiometricData) {
+        this.facialBiometricData = facialBiometricData;
+        this.fingerprintBiometricData = fingerprintBiometricData;
+    }
+
+    public SingleBiometricData getFacialBiometricData() {
+        return facialBiometricData;
+    }
+
+    public SingleBiometricData getFingerprintBiometricData() {
+        return fingerprintBiometricData;
+    }
 }
