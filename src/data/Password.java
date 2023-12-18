1556586a4630300e39b9d65fcc7b11cc4e3c7b88
bk203esd <bk203esd@gmail.com>
2023-12-18 09:39:45 +0100

    implemented Password.java
    (cherry picked from commit d41d2dbc065f8e3b2c78d73211318f52b713f75f)

--- src/data/Password.java
+++ src/data/Password.java
@@ -1,4 +1,19 @@
 package data;
 
 public class Password {
+    // Attributes
+    private final String password;
+
+    // Constructor
+    public Password(String password) {
+        if (password == null) {
+            throw new IllegalArgumentException("Password cannot be null");
+        }
+        this.password = password;
+    }
+
+    // Getters
+    public String getPassword() {
+        return password;
+    }
 }
