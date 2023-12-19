71bc0fadb82d0de241f2d3792ec6dbcf90bec0f9
loanarm2306 <lrm17@alumnes.udl.cat>
2023-12-19 10:38:56 +0100

    He casi completat el NifTest. Necessito que mirem la classe nif perquè al tenir mètodes privats no els puc cridar a la classe de tests.

--- test/data/NifTest.java
+++ test/data/NifTest.java
@@ -1,4 +1,42 @@
 package data;
 
+import exceptions.InvalidDNIDocumException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class NifTest {
+
+    private Nif validNif;
+    private Nif invalidNif;
+
+    @BeforeEach
+    public void setUp() {
+        // Valid NIF
+        try {
+            validNif = new Nif("Z34565434");
+        } catch (InvalidDNIDocumException e) {
+            throw new RuntimeException("Failed to set up valid NIF", e);
+        }
+
+        // Invalid NIF
+        try {
+            invalidNif = new Nif("3456711X");
+        } catch (InvalidDNIDocumException e) {
+            throw new RuntimeException("Failed to set up invalid NIF", e);
+        }
+    }
+
+    @Test
+    public void testValidNif() {
+        assertNotNull(validNif);
+        // assertEquals("Z34565434", validNif.validateNif);
+    }
+
+    @Test
+    public void testInvalidNif() {
+        assertNotNull(invalidNif);
+    }
 }
