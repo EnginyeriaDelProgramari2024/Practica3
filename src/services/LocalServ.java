01e2c0ec9eb416e045066b6e09b6e0590d458212
RobertaAlinaM <robertaalina7@gmail.com>
2023-12-28 21:14:28 +0100

    Created Scrutiny class

---
+++ src/services/LocalServ.java
@@ -0,0 +1,11 @@
+package services;
+
+import data.Password;
+import exceptions.InvalidAccountException;
+
+public class LocalServ implements LocalService{
+    @Override
+    public void verifyAccount(String login, Password pssw) throws InvalidAccountException {
+
+    }
+}
