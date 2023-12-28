12d92a194ba2727e4eae74f95b9f8ab1e0c2033c
RobertaAlinaM <robertaalina7@gmail.com>
2023-12-28 21:52:27 +0100

    Mistaken branch

--- src/data/BiometricData.java
+++ src/data/BiometricData.java
@@ -1,4 +1,13 @@
 package data;
 
 public class BiometricData {
+    private byte[] biometricKey;
+
+    public void SingleBiometricData(byte[] biometricKey) {
+        this.biometricKey = biometricKey;
+    }
+
+    public byte[] getBiometricKey() {
+        return biometricKey;
+    }
 }
