ba612f488341724383da6cf495b9c828c5986a57
RobertaAlinaM <robertaalina7@gmail.com>
2023-12-29 20:29:35 +0100

    Implemented canVote.

--- src/services/ElectoralOrg.java
+++ src/services/ElectoralOrg.java
@@ -8,6 +8,10 @@ public class ElectoralOrg implements ElectoralOrganism {
     @Override
     public void canVote(Nif nif) throws NotEnabledException, ConnectException {
 
+        if(nif == null) throw new ConnectException("Nif is null");
+        if(nif.getNif() == null) throw new ConnectException("Nif is null");
+        if(nif.getNif().length() != 9) throw new ConnectException("Nif is not valid");
+
     }
 
     @Override
