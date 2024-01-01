30d4cfb32de794a4e1f20e3b261c5f6219b9d5a3
bk203esd <bk203esd@gmail.com>
2024-01-01 15:41:20 +0100

    correcte HumanScanner.java

--- src/evoting/biometricdataperipheral/HumanScanner.java
+++ src/evoting/biometricdataperipheral/HumanScanner.java
@@ -3,14 +3,20 @@ package evoting.biometricdataperipheral;
 import data.SingleBiometricData;
 import exceptions.HumanBiometricScanningException;
 
-public class HumanScanner implements HumanBiometricScanner{
+public class HumanScanner implements HumanBiometricScanner {
     @Override
     public SingleBiometricData scanFaceBiometrics() throws HumanBiometricScanningException {
+        if (scanFaceBiometrics().getFacialBiometricData() == null) {
+            throw new HumanBiometricScanningException("Face biometrics could not be scanned.");
+        }
         return scanFaceBiometrics().getFacialBiometricData();
     }
 
     @Override
     public SingleBiometricData scanFingerprintBiometrics() throws HumanBiometricScanningException {
+        if (scanFingerprintBiometrics().getFingerprintBiometricData() == null) {
+            throw new HumanBiometricScanningException("Fingerprint biometrics could not be scanned.");
+        }
         return scanFingerprintBiometrics().getFingerprintBiometricData();
     }
 }
