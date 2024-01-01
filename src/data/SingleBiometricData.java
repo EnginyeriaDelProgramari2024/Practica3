1e39b9678f4857e5c393e5744fb79a314d72635d
bk203esd <bk203esd@gmail.com>
2024-01-01 22:19:21 +0100

    corrected SingleBiometricData.java

--- src/data/SingleBiometricData.java
+++ src/data/SingleBiometricData.java
@@ -10,14 +10,27 @@ package data;
 
 
 public class SingleBiometricData {
+    private byte[] biometricData;
+    // Select one of the following: "facial" or "fingerprint"
+    private String biometricType;
+
+    public SingleBiometricData(byte[] biometricData, String biometricType) {
+        this.biometricData = biometricData;
+        this.biometricType = biometricType;
+    }
+
 
     public SingleBiometricData getFacialBiometricData() {
-        //TODO: implement
+        if (biometricType.equals("facial")) {
+            return this;
+        }
         return null;
     }
 
     public SingleBiometricData getFingerprintBiometricData() {
-        //TODO: implement
+        if (biometricType.equals("fingerprint")) {
+            return this;
+        }
         return null;
     }
 }
