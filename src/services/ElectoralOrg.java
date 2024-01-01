f6cd36815d02259987f84a4ac33bd8f0d2fada88
bk203esd <bk203esd@gmail.com>
2024-01-01 16:51:44 +0100

    not needed to check if has voted

--- src/services/ElectoralOrg.java
+++ src/services/ElectoralOrg.java
@@ -8,12 +8,11 @@ public class ElectoralOrg implements ElectoralOrganism {
     @Override
     public void canVote(Nif nif) throws NotEnabledException, ConnectException {
 
-       // From the NIF, it verifies whether the voter meets all the conditions for voting, before proceeding to cast the vote.
+        // From the NIF, it verifies whether the voter meets all the conditions for voting, before proceeding to cast the vote.
 
-        if(nif == null) throw new ConnectException("Nif is null");
-        if(nif.getNif() == null) throw new ConnectException("Nif is null");
-        if(!Nif.validateNif(nif.getNif())) throw new NotEnabledException("Nif is not enabled");
-
+        if (nif == null) throw new ConnectException("Nif is null");
+        if (nif.getNif() == null) throw new ConnectException("Nif is null");
+        if (!Nif.validateNif(nif.getNif())) throw new NotEnabledException("Nif is not enabled");
     }
 
     @Override
@@ -22,11 +21,7 @@ public class ElectoralOrg implements ElectoralOrganism {
         //From the NIF, it is recorded in the electoral roll that said voter has already exercised the right to vote (the voter is disqualified, thus avoiding the possibility of duplicate votes)
 
         boolean voted = false;
-        //TODO: Check if the voter has already voted
-        if(canVote(nif)){
-            voted = true;
-        }
-        if( voted == true){
+        if (voted == true) {
             throw new ConnectException("Voter has already voted");
         }
     }
