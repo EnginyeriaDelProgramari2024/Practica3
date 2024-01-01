d431dce9c7b4ddb86b3c36de720278b443dc67ae
bk203esd <bk203esd@gmail.com>
2024-01-01 18:21:42 +0100

    correction ElectoralOrg.java

--- src/services/ElectoralOrg.java
+++ src/services/ElectoralOrg.java
@@ -5,6 +5,13 @@ import exceptions.ConnectException;
 import exceptions.NotEnabledException;
 
 public class ElectoralOrg implements ElectoralOrganism {
+    private boolean voted;
+
+    // Constructor
+    public ElectoralOrg() {
+        this.voted = false;
+    }
+
     @Override
     public void canVote(Nif nif) throws NotEnabledException, ConnectException {
 
@@ -19,10 +26,10 @@ public class ElectoralOrg implements ElectoralOrganism {
     public void disableVoter(Nif nif) throws ConnectException, NotEnabledException {
 
         //From the NIF, it is recorded in the electoral roll that said voter has already exercised the right to vote (the voter is disqualified, thus avoiding the possibility of duplicate votes)
-
-        boolean voted = false;
-        if (voted == true) {
-            throw new ConnectException("Voter has already voted");
-        }
+        if (nif == null) throw new ConnectException("Nif is null");
+        if (nif.getNif() == null) throw new ConnectException("Nif is null");
+        if (!Nif.validateNif(nif.getNif())) throw new NotEnabledException("Nif is not enabled");
+        if (this.voted) throw new NotEnabledException("Nif is not enabled");
+        this.voted = true;
     }
 }
