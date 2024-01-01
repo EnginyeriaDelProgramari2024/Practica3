17cbea6824320c1df1ee5ac91521da109530240e
loanarm2306 <lrm17@alumnes.udl.cat>
2024-01-01 10:35:17 +0100

    Classe Scrtny.java finalitzada. He decidit com a estructura de dades per guardar els vots un mapa amb una clau que es el partit. D'aquesta forma és molt més eficient.

--- src/services/Scrtny.java
+++ src/services/Scrtny.java
@@ -2,56 +2,71 @@ package services;
 
 import data.VotingOption;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class Scrtny implements Scrutiny{
+public class Scrtny implements Scrutiny {
 
-    private int totalVotes;
+    // Map to store the vote count for each party
+    private Map<VotingOption, Integer> voteCount;
+
+    // Counter for null votes, blank votes and total vote count
+    private int nullVotes;
     private int blankVotes;
+    private int totalVotes;
 
-    public Scrtny() {
-
-        this.totalVotes = 0;
-        this.blankVotes = 0;
-
-    }
     @Override
     public void initVoteCount(List<VotingOption> validParties) {
-
+        voteCount = new HashMap<>();
+        for (VotingOption party : validParties) {
+            voteCount.put(party, 0);
+        }
+        nullVotes = 0;
+        blankVotes = 0;
+        totalVotes = 0;
     }
 
     @Override
     public void scrutinize(VotingOption vopt) {
-
-        // Incorporates the voting option (vopt) in the count of votes.
+        if (voteCount.containsKey(vopt)) {
+            voteCount.put(vopt, voteCount.get(vopt) + 1);
+        } else if (vopt == null) {
+            nullVotes++;
+        } else {
+            blankVotes++;
+        }
+        totalVotes++;
     }
 
     @Override
     public int getVotesFor(VotingOption vopt) {
-        return 0;
-    }
-
-    @Override
-    public int getTotal() {
-
-        // Returns the total number of votes counted.
-        return totalVotes;
-
+        return voteCount.getOrDefault(vopt, 0);
     }
 
     @Override
     public int getNulls() {
-        return 0;
+        return nullVotes;
     }
 
     @Override
     public int getBlanks() {
-        // Returns the number of blank votes.
         return blankVotes;
     }
 
     @Override
-    public void getScrutinyResults() {
+    public int getTotal() {
+        return totalVotes;
+    }
 
+    @Override
+    public void getScrutinyResults() {
+        System.out.println("SCRUTINY RESULTS:");
+        for (Map.Entry<VotingOption, Integer> entry : voteCount.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue() + " votes");
+        }
+        System.out.println("Blank votes: " + blankVotes);
+        System.out.println("Null votes: " + nullVotes);
+        System.out.println("Total votes: " + totalVotes);
     }
 }
